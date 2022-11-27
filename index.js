94e2fd34a48a4bdd9b42aec8096d341f94d39a37
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2022-11-27 15:33:12 -0300

    add test var

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const cron = require('node-schedule');
 
+let test = 1;
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send(`Yo! ${test}`)
 })
 app.listen(process.env.PORT || 3000)
 
 cron.scheduleJob('*/5 * * * * *', () => {
+    test++;
     console.log(' MY CRON SAYS: "5 seconds has pass".')
 })
 
 cron.scheduleJob('*/60 * * * * *', () => {
     console.log(' MY CRON SAYS: "60 seconds has pass".')
 })
+
