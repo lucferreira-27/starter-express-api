7e11b12392a6e8107e284ab901c294cc796b8295
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2022-11-27 15:22:08 -0300

    add node schedule

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const cron = require('node-schedule');
+
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
+
+cron.scheduleJob('*/5 * * * * *', () => {
+    console.log(' MY CRON SAYS: "5 seconds has pass".')
+})
+
+cron.scheduleJob('*/60 * * * * *', () => {
+    console.log(' MY CRON SAYS: "60 seconds has pass".')
+})
