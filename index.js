03b95cb7512b81cb19ecfb0b42610684611a6949
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2022-11-29 12:25:10 -0300

    new versel template

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
 const express = require('express')
-import fetch from 'node-fetch';
-const dontpad = require('dontpad-api');
-const cron = require('node-schedule');
-
-let test = 1;
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send(`Yo! ${test}`)
+    res.send(`Yo! Hello World`)
 })
 app.listen(process.env.PORT || 3000)
 
-
-cron.scheduleJob('*/1 * * * * *', async () => {
-    console.log(test++, new Date().toLocaleTimeString());
-})
 
