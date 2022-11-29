d6e76c17168c9f3f4137f7b0a596212f0edcd88f
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2022-11-29 12:28:12 -0300

    new versel template

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+app.all('/hello', (req, res) => {
     console.log("Just got a request!")
     res.send(`Yo! Hello World`)
 })
