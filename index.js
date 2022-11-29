6b3d7f20729640c59a5f8c20cdb135c7e896cafc
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2022-11-29 13:30:12 -0300

    remove tasks

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 const express = require('express')
 const app = express()
 const time = 0
-const task = require('./tasks')
-task(time)
+//const task = require('./tasks')
+//task(time)
+schedule.scheduleJob('*/60 * * * * *', function () {
+    console.log('The answer to life, the universe, and everything!');
+    time++
+});
 app.all('/hello', (req, res) => {
     console.log("Just got a request!")
-    res.send(`Yo! Hello World`)
+    res.send(`Yo! Hello World ${time}`)
 })
 app.listen(process.env.PORT || 3000)
 
